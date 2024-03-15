db5a4ce69db7c16d450a839fbbc8ea71293480a5
ablake12 <ablake12@scmail.spelman.edu>
2024-03-15 02:42:09 -0400

    Create script.js

---
+++ website/static/script.js
@@ -0,0 +1,9 @@
+function activeFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+}
