27cf17dfd333b8c326f39f85cfaac264cf155dce
ablake12 <ablake12@scmail.spelman.edu>
2024-03-17 01:46:43 -0400

    Update website.py

--- website/website.py
+++ website/website.py
@@ -1,31 +1,31 @@
-from flask import Flask, request, render_template#import flask libraries
+from flask import Flask, render_template#import flask libraries
 
 
 app = Flask(__name__)#create Flask object
 
 
 @app.route('/')#make this function run on the original route
-def homeFunct():
+def home_funct():
     return render_template('home.html')#render html form
 
 @app.route('/about')#make this function run on the original route
-def aboutFunct():
+def about_funct():
     return render_template('about.html')#render html form
 
 @app.route('/resume')#make this function run on the original route
-def resumeFunct():
+def resume_funct():
     return render_template('resume.html')#render html form
 
 @app.route('/projects')#make this function run on the original route
-def projectFunct():
+def project_funct():
     return render_template('projects.html')#render html form
 
 @app.route('/contact')#make this function run on the original route
-def contactFunct():
+def contact_funct():
     return render_template('contact.html')#render html form
 
 @app.route('/urls')#make this function run on the original route
-def linkFunct():
+def link_funct():
     return render_template('links.html')#render html form
 
 
