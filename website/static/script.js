18260c4e7da81e7865d8d0d619f4f5411a9bbeef
ablake12 <ablake12@scmail.spelman.edu>
2024-12-11 03:09:37 -0500

    Update script.js

--- website/static/script.js
+++ website/static/script.js
@@ -1,9 +1,9 @@
-function activeFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    }
-    else {
-        x.className = "topnav";
-    }
-}
+// function activeFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     }
+//     else {
+//         x.className = "topnav";
+//     }
+// }
