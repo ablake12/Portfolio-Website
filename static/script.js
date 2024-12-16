c63a261df944584283daae594f82290f49f3f5e3
ablake12 <ablake12@scmail.spelman.edu>
2024-12-16 11:36:13 -0500

    Add mobile nav bar functionality

--- static/script.js
+++ static/script.js
@@ -1,9 +1,18 @@
-// function activeFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     }
-//     else {
-//         x.className = "topnav";
-//     }
-// }
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggle = document.querySelector('.navbar-header a'); // Your toggle link
+//     const collapsibleMenu = document.querySelector('.navbar-collapse');
+
+//     toggle.addEventListener('click', function (event) {
+//         event.preventDefault(); // Prevent the link's default behavior
+//         collapsibleMenu.classList.toggle('show'); // Toggle the visibility
+//     });
+// });
+
+const collapseBtn = document.querySelector('.mobile-nav-toggle');
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    collapseBtn.classList.toggle('bi-x');
+    collapseBtn.classList.toggle('bi-list');
+  }
+  collapseBtn.addEventListener('click', mobileNavToogle);
