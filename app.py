d70f9cf9138da460c12fcec2479abb809e419130
ablake12 <ablake12@scmail.spelman.edu>
2025-01-02 00:37:31 -0500

    Add comments

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, send_file #import flask libraries
-from flask_mail import Mail, Message #import flask library for email
+from flask import Flask, render_template, request, send_file # import flask libraries
+from flask_mail import Mail, Message # import flask library for email
 
 load_dotenv()
 
-app = Flask(__name__)#create Flask object
+app = Flask(__name__)# create Flask object
 mail = Mail(app) # initialize mail class
    
-# configuration of mail 
+# Mail configuration
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
@@ -18,7 +18,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
-@app.route('/')#make this function run on the original route
+@app.route('/')# make this function run on the original route
 def home():
     return render_template('home.html')#render html form
 
@@ -49,6 +49,7 @@ def contact():
 @app.route('/contact', methods = ['POST'])# html template for after submitting the form
 def submitted():
     try:
+        # retrieve answers from html form
         name = request.form.get("name")
         email = request.form.get("email")
         subject = request.form.get("subject")
@@ -60,6 +61,7 @@ def submitted():
         # else:
         #     subject = f"Incoming Message From Website - {subject}"
 
+        # set recipient as self, reply to set to the actual recipient
         msg = Message(subject, recipients=[os.getenv('EMAIL_USERNAME')], reply_to=email)
         msg.body = f"Name: {name}\n\nEmail: {email}\n\nMessage:\n{message}"
         mail.send(msg)
@@ -70,4 +72,4 @@ def submitted():
         return render_template('submitted.html', return_message = "Your message failed to submit due to an internal error. While I work to fix this issue, please reach out via email directly by clicking on the mail icon on the left side of this page. Thank you and I apologize for the inconvenience.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # set to false in final commit
